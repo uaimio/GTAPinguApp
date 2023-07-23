2cc1891eb9e4cf95835fa88b38d97f515c17635a
uaimio <mario.acocella@outlook.com>
2023-07-23 13:21:34 +0200

    fix planned activities

--- planned_activities.sh
+++ planned_activities.sh
@@ -4,4 +4,4 @@
 0 6 1-31 * * rm $HOME/GTAPinguApp/*.webm >> $HOME/GTAPinguApp/logs/signal.log 2>> $HOME/GTAPinguApp/logs/signal.log
 
 # resetting morio cho every day
-0 5 1-31 * * kill -SIGUSR1 $(pidof "bot.py") >> $HOME/GTAPinguApp/logs/signal.log 2>> $HOME/GTAPinguApp/logs/signal.log
+0 5 1-31 * * kill -SIGUSR1 $(pidof -x bot.py) >> $HOME/GTAPinguApp/logs/signal.log 2>> $HOME/GTAPinguApp/logs/signal.log
