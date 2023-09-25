9f6024e4f8a6f127e7b0c7cb7ceff73fc4204a88
uaimio <mario.acocella@outlook.com>
2023-09-25 23:10:13 +0200

    minor fix

--- planned_activities.sh
+++ planned_activities.sh
@@ -4,4 +4,4 @@
 0 6 1-31 * * "$HOME/GTAPinguApp/garbage_collector.sh" "$HOME/GTAPinguApp/logs/deleted.log"
 
 # resetting morio cho every day
-0 5 1-31 * * kill -s SIGUSR1 $(pidof -x bot.py) >> $HOME/GTAPinguApp/logs/signal.log 2>> $HOME/GTAPinguApp/logs/signal.log
+0 5 1-31 * * kill -s SIGUSR1 $(pidof -x bot.py) >> "$HOME/GTAPinguApp/logs/signal.log" 2>> "$HOME/GTAPinguApp/logs/signal.log"
