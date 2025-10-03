778321ad0429a868f89488b9c9e0c9bb27dcadc7
Peenguino <giuseppe_acocella@outlook.it>
2025-10-03 22:07:04 +0200

    fix: update script to use bash and correct python command

--- bot_start_local.sh
+++ bot_start_local.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
+mkdir -p logs
 DATETIME=$(date '+%Y-%m-%d_%H-%M-%S')
 LOGFILE="./logs/logs_${DATETIME}.txt"
 
-python ./bot.py > "$LOGFILE" >2 "$LOGFILE" & disown
+python3 ./bot.py >> "$LOGFILE" >>2 "$LOGFILE" & disown
